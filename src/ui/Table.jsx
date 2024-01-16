ef67210b2ff5d10ba795ee84685d4969b6998519
Stephen <stephennguyen24@gmail.com>
2024-01-16 21:36:43 +0700

    Refactor table body

--- src/ui/Table.jsx
+++ src/ui/Table.jsx
@@ -73,7 +73,7 @@ export default function Table({ columns, children }) {
 function Header({ children }) {
   const { columns } = useContext(TableContext);
   return (
-    <StyledHeader role="row" $columns={columns}>
+    <StyledHeader role="row" $columns={columns} as="header">
       {children}
     </StyledHeader>
   );
@@ -86,7 +86,10 @@ function Row({ children }) {
     </StyledRow>
   );
 }
-function Body({ children }) {}
+function Body({ data, render }) {
+  if (!data.length) return <Empty>No data to show at the moment</Empty>;
+  return <StyledBody>{data.map(render)}</StyledBody>;
+}
 
 Table.Header = Header;
 Table.Body = Body;
