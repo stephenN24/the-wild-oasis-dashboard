e4206291759f0188f33c03edfa68c455a832ac59
Stephen <stephennguyen24@gmail.com>
2024-01-17 22:05:32 +0700

    Implement sort

--- src/features/cabins/CabinTableOperations.jsx
+++ src/features/cabins/CabinTableOperations.jsx
@@ -1,5 +1,6 @@
 import TableOperations from "../../ui/TableOperations";
 import Filter from "../../ui/Filter";
+import SortBy from "../../ui/SortBy";
 
 function CabinTableOperations() {
   return (
@@ -10,6 +11,19 @@ function CabinTableOperations() {
           { value: "all", label: "All" },
           { value: "no-discount", label: "No discount" },
           { value: "with-discount", label: "With discount" },
+        ]}
+      />
+      <SortBy
+        options={[
+          { value: "name-asc", label: "Alphabetically (A-Z)" },
+          { value: "name-desc", label: "Alphabetically (Z-A)" },
+          { value: "regularPrice-asc", label: "Price (Low to High)" },
+          { value: "regularPrice-desc", label: "Price (High to Low)" },
+          { value: "maxCapacity-asc", label: "Capacity (Low to High)" },
+          {
+            value: "maxCapacity-desc",
+            label: "Capacity (High to Low)",
+          },
         ]}
       />
     </TableOperations>
--- src/ui/Select.jsx
+++ src/ui/Select.jsx
@@ -5,11 +5,25 @@ const StyledSelect = styled.select`
   padding: 0.8rem 1.2rem;
   border: 1px solid
     ${(props) =>
-      props.type === "white"
+      props.$type === "white"
         ? "var(--color-grey-100)"
         : "var(--color-grey-300)"};
   border-radius: var(--border-radius-sm);
   background-color: var(--color-grey-0);
-  font-weight: 500;
+  font-weight: 600;
   box-shadow: var(--shadow-sm);
 `;
+
+function Select({ options, value, onchange, ...props }) {
+  return (
+    <StyledSelect value={value} {...props} onchange={onchange}>
+      {options.map((option) => (
+        <option key={option.value} value={option.value}>
+          {option.label}
+        </option>
+      ))}
+    </StyledSelect>
+  );
+}
+
+export default Select;
