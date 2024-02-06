f71c2859240407309889884d4508eff67fff5b6f
Stephen <stephennguyen24@gmail.com>
2024-02-06 22:27:57 +0700

    feat(error): add error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ErrorBoundary
+      fallback={<div>Something went wrong</div>}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
+  </React.StrictMode>
+);
