d2e809b0d4edd9f69b7de3d6804bcd9d6445944f
Stephen <141339273+stephenN24@users.noreply.github.com>
2024-07-08 19:31:49 +0700

    chore(authentication): update password

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -8,7 +8,7 @@ import { useLogin } from "./useLogin.js";
 
 function LoginForm() {
   const [email, setEmail] = useState("thuong@gmail.com");
-  const [password, setPassword] = useState("123456");
+  const [password, setPassword] = useState("12345678");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
