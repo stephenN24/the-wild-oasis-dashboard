d915024596cb1380e1038110b5e480adb47f08cf
Stephen <141339273+stephenN24@users.noreply.github.com>
2024-07-08 19:29:45 +0700

    chore(authentication): add default account

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import SpinnerMini from "../../ui/SpinnerMini.jsx";
 import { useLogin } from "./useLogin.js";
 
 function LoginForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("thuong@gmail.com");
+  const [password, setPassword] = useState("123456");
   const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
